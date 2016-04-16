03fa6cd19f73d249a6f3199c0b8874238480bb74
GPointExtreme <dominik.geiger@hotmail.com>
2016-04-16 14:18:47 +0200

    Weiter coden und keinen Plan haben was ich da vor einem Monat gemacht habe...

--- src/exceptions/ue/account/Bank.java
+++ src/exceptions/ue/account/Bank.java
@@ -2,6 +2,8 @@ package exceptions.ue.account;
 
 import java.util.HashMap;
 
+import javax.security.auth.login.AccountNotFoundException;
+
 public class Bank {
 	
 	private HashMap<String, Account> accounts = new HashMap<>();
@@ -13,5 +15,24 @@ public class Bank {
 		}
 		accounts.put(owner, new Account(owner, balance, overdraftFrame));
 	}
+	
+	public void transfer(String fromOwner, String toOwner, double amount)
+			throws AccountNotFoundException, BankException {
+		if(!accounts.containsKey(fromOwner) || !accounts.containsKey(toOwner)) {
+			throw new AccountNotFoundException("No Owner found!");
+		}
+		try {
+			accounts.get(fromOwner).depit(amount);
+			accounts.get(fromOwner).depit(amount);
+		}
+		catch(NotEnoughMoneyException exc) {
+			throw new BankException("Not enough Money!", exc);
+		}
+	}
 
+	@Override
+	public String toString() {
+		return "Bank [accounts=" + accounts + "]";
+	}
+	
 }
